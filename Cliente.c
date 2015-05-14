f6f9174ee0b02a5dc3bfe8f11b17598a20a4f6e3
Bfzanchetta <bfzanchetta@hotmail.com>
2015-05-14 18:06:26 -0300

    Update Cliente.c

--- Cliente.c
+++ Cliente.c
@@ -1,17 +1,18 @@
-/* tcpclient.c */
+/* tcpserver.c */
 
-#include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
+#include <arpa/inet.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
+#include <netdb.h>
 
 
-void consultaDNS(){				//funcao pelo qual o cliente se comunica com o DNS
+void informaDNS(){
 	int sock, bytes_recieved;  
         char send_data[1024],recv_data[1024];
         struct hostent *host;
@@ -22,7 +23,7 @@ void consultaDNS(){				//funcao pelo qual o cliente se comunica com o DNS
         if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
             perror("Socket");
             exit(1);
-        }
+        }	
 
         server_addr.sin_family = AF_INET;     
         server_addr.sin_port = htons(9005);   
@@ -35,101 +36,116 @@ void consultaDNS(){				//funcao pelo qual o cliente se comunica com o DNS
             perror("Connect");
             exit(1);
         }
-
-        while(1)			//comeco da troca de msg
-        {
-        
-           printf("\nProcura IP ou Numero: ");   //vai enviar uma das 2 opcoes, o DNS distingue as msgs e retorna os IPs
-           printf("\nDigite IP pra procurar os IPs, ou digite 2p, 3p ou 4p pra visualizar os IPs com esses numeros de players");
+	
+           printf("\nQuantos jogadores seu servidor tem? ");
+	   printf("2, 3 ou 4?");
            gets(send_data);
            
-          if (strcmp(send_data , "q") != 0 && strcmp(send_data , "Q") != 0)
+          
            send(sock,send_data,strlen(send_data), 0); 
-
-          else
-          {
-           send(sock,send_data,strlen(send_data), 0);   //envia a opcao escolhida
-          }
 	  
 
           bytes_recieved=recv(sock,recv_data,1024,0);
           recv_data[bytes_recieved] = '\0';
-	  
+ 
 
-	  printf("%c",&recv_data);
+          
+	if(strcmp(recv_data , "IP anotado") == 0){
+			printf("\nServidor registrado com sucesso, aguardando clientes");
+           		}
+	else{
+			printf("Mensagem nao reconhecida");
+			
+	}
+          
+
+	//close(sock);
 	
-        close(sock);
-	break;
-        }   
-
 }
 
 
 
-
 int main()
-
-
 {
-	//void consultaDNS();	
-	char* ipDesejado[15];
-        int sock, bytes_recieved;  
-        char send_data[1024],recv_data[1024];
-        struct hostent *host;
-        struct sockaddr_in server_addr;  
+	informaDNS();
+        int sock, connected, bytes_recieved , true = 1;  
+        char send_data [1024] , recv_data[1024];       
 
-        host = gethostbyname("192.168.1.29");
-
+        struct sockaddr_in server_addr,client_addr;    
+        int sin_size;
+        
         if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
             perror("Socket");
             exit(1);
         }
 
-	printf("Digite o server que deseja 23213se conectar:");
-	//scanf("%c","192.168.1.72");
-
-        server_addr.sin_family = AF_INET;     
-        server_addr.sin_port = htons(9006);  	//Se conecta ao ip escrito 
-        server_addr.sin_addr = *((struct in_addr *)host->h_addr);
+        if (setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,&true,sizeof(int)) == -1) {
+            perror("Setsockopt");
+            exit(1);
+        }
+        
+        server_addr.sin_family = AF_INET;         
+        server_addr.sin_port = htons(9006);     
+        server_addr.sin_addr.s_addr = INADDR_ANY; 
         bzero(&(server_addr.sin_zero),8); 
 
-        if (connect(sock, (struct sockaddr *)&server_addr,
-                    sizeof(struct sockaddr)) == -1) 
-        {
-            perror("Connect");
+        if (bind(sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr))
+                                                                       == -1) {
+            perror("Unable to bind");
             exit(1);
         }
 
+        if (listen(sock, 5) == -1) {
+            perror("Listen");
+            exit(1);
+        }
+		
+	printf("\nRegular game room waiting for player on port 9006");
+        fflush(stdout);
+
+	
         while(1)
-        {
-        
-          
-           printf("\nDigite a mensagem ou (q or Q to quit) : ");
-           gets(send_data);
-           
-          if (strcmp(send_data , "q") != 0 && strcmp(send_data , "Q") != 0)
-           send(sock,send_data,strlen(send_data), 0); 
+        {  
 
-          else
-          {
-           send(sock,send_data,strlen(send_data), 0);   
-           close(sock);
-           break;
-          }
+            sin_size = sizeof(struct sockaddr_in);
 
-	  bytes_recieved=recv(sock,recv_data,1024,0);
-          recv_data[bytes_recieved] = '\0';
- 
-          if (strcmp(recv_data , "q") == 0 || strcmp(recv_data , "Q") == 0)
-          {
-           close(sock);
-           break;
-          }
+            connected = accept(sock, (struct sockaddr *)&client_addr,&sin_size);
 
-          else
-           printf("\nRecieved data = %s " , recv_data);
-           
-        
-        }   
-return 0;
+            printf("\n I got a connection from (%s , %d)",
+                   inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
+
+            while (1)
+            {
+              printf("\n SEND (q or Q to quit) : ");
+              gets(send_data);
+              
+              if (strcmp(send_data , "q") == 0 || strcmp(send_data , "Q") == 0)
+              {
+                send(connected, send_data,strlen(send_data), 0); 
+                close(connected);
+                break;
+              }
+               
+              else
+                 send(connected, send_data,strlen(send_data), 0);  
+
+              bytes_recieved = recv(connected,recv_data,1024,0);
+
+              recv_data[bytes_recieved] = '\0';
+
+              if (strcmp(recv_data , "q") == 0 || strcmp(recv_data , "Q") == 0)
+              {
+                close(connected);
+                break;
+              }
+
+              else 
+              printf("\n RECIEVED DATA = %s " , recv_data);
+              fflush(stdout);
+            }
+        }       
+
+      close(sock);
+      return 0;
 }
+
